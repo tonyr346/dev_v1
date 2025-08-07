1e7f5648d51cb24cbb508a206ea111355f5f5df3
tonyr346 <anthony@roggenbuck.us>
2025-08-07 12:44:58 -0400

    8282

---
+++ MQconfig.h
@@ -0,0 +1,18 @@
+       
+#define DEVICE_ID  "AX01RPI5"
+#define SUB_ID "AX0346pi"
+#define MQTT_BROKER "ssl://cbd9275329ce4ff3a66e657e089c16f2.s1.eu.hivemq.cloud:8883"
+#define MQTT_PORT 8883
+#define MQTT_U "tr346"
+#define MQTT_P "TR@346_zxr"
+
+#define WIFI_NET "TR Wifi"
+#define WIFI_PASS "5867034894"
+
+// Database configuration
+#define DB_USER "ax12"
+#define DB_PASS "Zdbdab2325x"
+#define SQL_USER "sg54ni1t35fbwc59_ax12"
+#define SQL_PASS DB_PASS
+#define DB_NAME "sg54ni1t35fbwc59_AXIDAT"
+#define DB_HOST "https://axicoms.axieng.com"
