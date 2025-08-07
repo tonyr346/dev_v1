d393de90c8fe29c4cbcece11dff938d38c890442
tonyr346 <anthony@roggenbuck.us>
2025-08-07 12:49:12 -0400

    933943

--- sky.c
+++ sky.c
@@ -1,40 +1,16 @@
-/*
- * sky.c
- * 
- * Copyright 2025  <tr346@raspberrypi>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
-	
-	int a = 5;
-	printf("enter a number");
-	int b = scanf();
-	
-	int c = a+b;
-	
-	printf(c);
-	
-	return 0;
-}
+    int a = 5;
+    int b;
 
+    printf("Enter a number: ");
+    scanf("%d", &b);
+
+    int c = a + b;
+
+    printf("Result (a + b = %d)\n", c);
+
+    return 0;
+}
