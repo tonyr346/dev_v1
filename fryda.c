c591c737185878a287e94b68eabe9dfedc72ae06
tonyr346 <anthony@roggenbuck.us>
2025-08-15 14:16:48 -0400

    0874087340

---
+++ fryda.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+
+int main() {
+
+    printf("fryda.c - JSON Read - START\n\n");
+
+
+    printf("<<<<DOING STUFF>>>>....\n\n");
+
+    delay(3000);
+
+
+    printf("fryda.c - JSON Read - END\n\n");
+      
+    return 0;
+
+}
